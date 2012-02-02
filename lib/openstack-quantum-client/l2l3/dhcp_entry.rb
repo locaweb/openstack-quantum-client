7186089b006f48a9a8f7df4c270a8ca392d918c2
PotHix <willian.molinari@locaweb.com.br>
2012-02-02 21:26:59 -0200

    Adding dhcp entry name.

--- lib/openstack-quantum-client/l2l3/dhcp_entry.rb
+++ lib/openstack-quantum-client/l2l3/dhcp_entry.rb
@@ -6,10 +6,10 @@ module Openstack
         @quantum_url = "#{quantum_url}/dhcp_entries.json"
       end
 
-      def create(address, mac)
+      def create(address, mac, name)
         post_to_quantum(
           @quantum_url,
-          {"dhcp_entry" => {"mac" => mac, "address" => address}}
+          {"dhcp_entry" => {"mac" => mac, "address" => address, "name" => name}}
         )
       end
     end
